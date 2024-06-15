f4a1d02be722a04095c5b5eae0a0a88891db7a79
Wesley Mutyambizi <wesleymutyambizi@gmail.com>
2024-06-15 23:13:41 +0200

    SingleActorPage

---
+++ moviewebsite/src/Pages/ActorPg/Actor.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Actor = ({ actorId }) => {
+  const [actor, setActor] = useState(null);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchActorData = async () => {
+      const actorResponse = await axios.get(`https://api.themoviedb.org/3/person/${actorId}?api_key=YOUR_API_KEY`);
+      setActor(actorResponse.data);
+    };
+
+    const fetchActorMovies = async () => {
+      const moviesResponse = await axios.get(`https://api.themoviedb.org/3/person/${actorId}/movie_credits?api_key=YOUR_API_KEY`);
+      setMovies(moviesResponse.data.cast);
+    };
+
+    fetchActorData();
+    fetchActorMovies();
+  }, [actorId]);
+
+  if (!actor) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{actor.name}</h2>
+      <img src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`} alt={actor.name} />
+      <p>Gender: {actor.gender === 1 ? 'Female' : 'Male'}</p>
+      <p>Popularity: {actor.popularity}</p>
+      <p>Birthday: {actor.birthday}</p>
+      <p>Biography: {actor.biography}</p>
+      <h3>Movies</h3>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Actor;
