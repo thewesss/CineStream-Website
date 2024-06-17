ca36103a7f0cd1a13c2ec86933fb80e54112b9b9
Wesley Mutyambizi <wesleymutyambizi@gmail.com>
2024-06-17 16:15:35 +0200

    GenrePage+Otherchanges

--- moviewebsite/src/components/GenreDropDown.jsx
+++ moviewebsite/src/components/GenreDropDown.jsx
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 const GenreDropdown = () => {
   const [genres, setGenres] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=(<<-Api-key->>)');
+    const fetchGenres = async () => {
+      const response = await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=<<-API_KEY->>');
       setGenres(response.data.genres);
     };
 
-    fetchData();
+    fetchGenres();
   }, []);
+
+  const handleGenreChange = (event) => {
+    const genreId = event.target.value;
+    history.push(`/genre/${genreId}`);
+  };
 
   return (
     <div>
-      <h2>Genre</h2>
-      <select>
+      <select onChange={handleGenreChange}>
+        <option value="">Select a genre</option>
         {genres.map((genre) => (
           <option key={genre.id} value={genre.id}>{genre.name}</option>
         ))}
