974fb8650415bf610a5c7ebd37eb9ba2e832b0a4
Wesley Mutyambizi <wesleymutyambizi@gmail.com>
2024-06-20 10:30:52 +0200

    UpdatedGenres

--- src/Pages/GenrePg/Genres.jsx
+++ src/Pages/GenrePg/Genres.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { apiKey } from '../../Util/API'; // wes
 
 const GenreMovies = () => {
   const { genreId } = useParams();
@@ -8,23 +9,33 @@ const GenreMovies = () => {
 
   useEffect(() => {
     const fetchMovies = async () => {
-      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=<<-API_KEY->>&with_genres=${genreId}`);
+      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}`);
       setMovies(response.data.results);
+      console.log(response.data.results);
+      
     };
 
     fetchMovies();
   }, [genreId]);
 
   return (
-    <div>
+    
+    <div key={movies.id}>
       <h2>Movies in Genre</h2>
       <ul>
         {movies.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
+          <div key={movie.id}>
+            <Link to={`/movie/${movies.id}`}>
+            <li>{movie.title}</li>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+            </Link>
+          </div>
         ))}
       </ul>
+
     </div>
   );
 };
 
 export default GenreMovies;
+
