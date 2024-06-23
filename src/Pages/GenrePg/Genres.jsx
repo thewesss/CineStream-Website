aa565b980f57e8120657e01318035cb906617c88
Wesley Mutyambizi <wesleymutyambizi@gmail.com>
2024-06-23 21:28:53 +0200

    Finalactors&genrePg

--- src/Pages/GenrePg/Genres.jsx
+++ src/Pages/GenrePg/Genres.jsx
@@ -1,41 +1,93 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
-import { apiKey } from '../../Util/API'; // wes
+import { apiKey } from '../../Util/API';
+import './Genre.css';
+
+const genreNames = {
+  28: 'Action',
+  12: 'Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'History',
+  27: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  878: 'Sci-fi',
+  10770: 'TV Movie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western'
+};
 
 const GenreMovies = () => {
   const { genreId } = useParams();
   const [movies, setMovies] = useState([]);
+  const [genreName, setGenreName] = useState('Movies in Genre');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const fetchMovies = async () => {
-      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}`);
+      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}&page=${currentPage}`);
       setMovies(response.data.results);
+      setTotalPages(response.data.total_pages);
       console.log(response.data.results);
-      
     };
 
     fetchMovies();
-  }, [genreId]);
+
+    if (genreNames[genreId]) {
+      setGenreName(genreNames[genreId]);
+    } else {
+      setGenreName('Movies in Genre');
+    }
+  }, [genreId, currentPage]);
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   return (
-    
-    <div key={movies.id}>
-      <h2>Movies in Genre</h2>
-      <ul>
+    <div className='movie-container'>
+      <h2 className='heading'>{genreName}</h2>
+      <div className="movies-grid">
         {movies.map((movie) => (
-          <div key={movie.id}>
-            <Link to={`/movie/${movies.id}`}>
-            <li>{movie.title}</li>
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+          <div key={movie.id} className="movie-card">
+            <Link to={`/movie/${movie.id}`}>
+              <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
             </Link>
+            <Link to={`/movie/${movie.id}`} className="movie-title">{movie.title}</Link>
           </div>
         ))}
-      </ul>
-
+      </div>
+      <div className="pagination">
+        <button onClick={handlePreviousPage} disabled={currentPage === 1}>Previous</button>
+        <span>Page {currentPage} of {totalPages}</span>
+        <button className='btn' onClick={handleNextPage} disabled={currentPage === totalPages}>Next</button>
+      </div>
     </div>
   );
 };
 
 export default GenreMovies;
+
+
+
+
+
 
