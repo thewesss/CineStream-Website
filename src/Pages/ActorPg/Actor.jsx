2d65bb3e4c1a9900a0d98c3fcfc4a7f5101685a3
Wesley Mutyambizi <wesleymutyambizi@gmail.com>
2024-06-23 21:20:05 +0200

    Actorfiles

--- src/Pages/ActorPg/Actor.jsx
+++ src/Pages/ActorPg/Actor.jsx
@@ -1,45 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams, Link } from 'react-router-dom';
+import { apiKey } from '../../Util/API';
+import './Actor.css';
 
-const Actor = ({ actorId }) => {
+const Actor = () => {
+  const { actorId } = useParams();
   const [actor, setActor] = useState(null);
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    const fetchActorData = async () => {
-      const actorResponse = await axios.get(`https://api.themoviedb.org/3/person/${actorId}?api_key=(<<-Api_key->>)`);
-      setActor(actorResponse.data);
+    const fetchActorDetails = async () => {
+      try {
+        const response = await axios.get(`https://api.themoviedb.org/3/person/${actorId}?api_key=${apiKey}`);
+        setActor(response.data);
+      } catch (error) {
+        console.error("Error fetching actor details:", error);
+      }
     };
 
     const fetchActorMovies = async () => {
-      const moviesResponse = await axios.get(`https://api.themoviedb.org/3/person/${actorId}/movie_credits?api_key=(<<-API_KEY->>`);
-      setMovies(moviesResponse.data.cast);
+      try {
+        const response = await axios.get(`https://api.themoviedb.org/3/person/${actorId}/movie_credits?api_key=${apiKey}`);
+        setMovies(response.data.cast);
+      } catch (error) {
+        console.error("Error fetching actor movies:", error);
+      }
     };
 
-    fetchActorData();
+    fetchActorDetails();
     fetchActorMovies();
   }, [actorId]);
 
-  if (!actor) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <div>
-      <h2>{actor.name}</h2>
-      <img src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`} alt={actor.name} />
-      <p>Gender: {actor.gender === 1 ? 'Female' : 'Male'}</p>
-      <p>Popularity: {actor.popularity}</p>
-      <p>Birthday: {actor.birthday}</p>
-      <p>Biography: {actor.biography}</p>
-      <h3>Featured Movies</h3>
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
+    <div className="actor-details-container">
+      {actor && (
+        <div className="actor-info">
+          <img src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`} alt={actor.name} />
+          <div className="actor-details">
+            <h1>{actor.name}</h1>
+            <p><strong>Gender:</strong> {actor.gender === 1 ? 'Female' : 'Male'}</p>
+            <p><strong>Popularity:</strong> {actor.popularity}</p>
+            <p><strong>Birthday:</strong> {actor.birthday}</p>
+            <div className="biography">
+              <h2>Biography:</h2>
+              <p className='bio'>{actor.biography}</p>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="movies-list">
+        <h2>Featured Movies</h2>
+        <div className="title-cards">
+          <div className="card-list">
+            {movies.map((movie) => (
+              <Link to={`/movie/${movie.id}`} key={movie.id} className="card">
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Actor;
+
+
+
+
